b4863ca46dba1641d6c96f2fda3f1106c6469892
ZIZINGA342 <129368350+ZIZINGA342@users.noreply.github.com>
2023-05-12 23:04:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Assignment_1
+Write a function that computes the power of two numbers
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int power() {
+  return //use pow() function
+}
+
+
